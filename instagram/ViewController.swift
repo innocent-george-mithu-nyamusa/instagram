99dec7d19bbef3deff0d5788ff608fc25cf85023
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2022-03-13 18:01:19 +0200

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Mithu's Macbook on 13/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
